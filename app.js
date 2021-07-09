9feb36a25d4b0791398af191a679d002e8862d20
umutinceer <mumutinceer@gmail.com>
2021-07-09 23:04:21 +0300

    code rewritten

--- app.js
+++ app.js
@@ -1,112 +1,82 @@
+let selectedOperator;
+let selectedNum = "";
+let selectedNum2;
+
+
+
+const display = document.querySelector("#display");
+
+/* INPUTS TO NUMPAD ARE RECORDED TO DISPLAY AND SAVED IN A VARIABLE*/
+const numpadButtons = document.querySelectorAll(".numpad");
+for(let i=0; i<numpadButtons.length; i++)
+{
+    let numpadList = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0'];
+
+    numpadButtons[i].addEventListener("click", () => {
+        selectedNum += numpadList[i];
+        display.textContent = selectedNum;
+    });
+}
+
+/*  INPUTS TO OPERATOR BUTTONS ARE RECORDED AND DISPLAYED */
+const operatorButtons = document.querySelectorAll(".operator");
+for(let i=0; i<operatorButtons.length; i++)
+{
+    let operatorList = ['/', '*', '-', '+'];
+    let displayedOperatorList = ['÷', '×', '-', '+'];
+
+    operatorButtons[i].addEventListener("click", () => {
+        selectedOperator = operatorList[i];
+        display.textContent = displayedOperatorList[i];
+        
+    });
+}
+
+/* WHEN CLICKED ON EQUAL BUTTON EQUATION IS EVALUATED AND RESULT IS DISPLAYED*/
+const equalsButton = document.querySelector("#btn-equals");
+equalsButton.addEventListener("click", () => {
+    console.log("selectednum = ",selectedNum);
+    console.log("selectedope= ",selectedOperator);
+    display.textContent = operate(selectedOperator,selectedNum, selectedNum);
+    console.log("fasda");
+});
+
+
 function add(x,y)
 {
-    return x+y;
+    return (+x)+(+y);
 }
 
 function substract(x,y)
 {
-    return x-y;
+    return (+x)-(+y);
 }
 
 function multiply(x,y)
 {
-    return x*y;
+    return (+x)*(+y);
 }
 
 function divide(x,y)
 {
-    return x/y;
+    return (+x)/(+y);
 }
 
 function operate(operator,num1,num2)
 {
     switch(operator)
     {
-        case "/":
-            return divide(num1,num2);
+        case '+':
+            return add(num1,num2);
             break;
-        
-        case "*":
-            return multiply(num1,num2);
-            break;
-
-        case "-":
+        case '-':
             return substract(num1,num2);
             break;
-        
-        case "+":
-            return add(num1,num2);
+        case '*':
+            return multiply(num1,num2);
+            break;
+        case '/':
+            return divide(num1,num2);
             break;
     }
 }
-
-let selectedOperator;
-
-function selectOperator(op)
-{
-    return op;
-}
-
-const operatorButtons = document.querySelectorAll(".operator");
-
-console.log(operatorButtons)
-
-for(let i=0; i<operatorButtons.length; i++)
-{
-    let operatorList = ['/','*','-','+']
-    operatorButtons[i].addEventListener("click", () => {
-        selectOperator(operatorList[i]);
-        //selectedOperator = operatorList[i];
-        //BURADA SEÇİLDİKTEN SONRA FONKSİYON ÇAĞIRILACAK
-    });
-}
-
-let selectedNumber = "";
-
-const display = document.querySelector("#display");
-
-let displayValue;
-
-function selectNumber(num)
-{
-    selectedNumber += num;
-    display.textContent = selectedNumber;
-    displayValue = selectedNumber;
-    console.log(selectedNumber)
-}
-
-
-const numberButtons = document.querySelectorAll(".numpad");
-
-console.log(numberButtons)
-
-for(let i=0; i<numberButtons.length; i++)
-{
-    let numpadList = ['7', '8', '9', '4', '5', '6', '1', '2', '3', '.', '0'];
-    numberButtons[i].addEventListener("click", () =>{
-        selectNumber(numpadList[i]);
-    });
-}
-
-
-
-const equalButton = document.querySelector("#btn-equals");
-
-equalButton.addEventListener("click", () => {
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
